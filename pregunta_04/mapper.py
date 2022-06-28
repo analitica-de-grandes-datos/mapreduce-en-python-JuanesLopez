98bd35b96ea6ff2d970874355290fa6b44f0994b
juaelopezlop <juaelopezlop@unal.edu.co>
2022-06-28 01:07:46 -0500

    p1-6

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,23 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    db = [line.split('   ') for line in sys.stdin]
+    col1 = [db[i][0] for i in range(len(db))]
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+    for reg in col1:
+
+            #
+            # escribe al flujo estandar de salida
+            #
+        sys.stdout.write("{}\t1\n".format(reg))
--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    db = [line.replace('\n', '') for line in sys.stdin]
+    db = [line.split('   ') for line  in db]
+    letras = [db[i][0] for i in range(len(db))]
+    valores = [db[i][2] for i in range(len(db))]
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+    for i in range(len(letras)):
+
+            #
+            # escribe al flujo estandar de salida
+            #
+        sys.stdout.write("{}\t{}\n".format(letras[i], valores[i]))
